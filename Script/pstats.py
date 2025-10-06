2543b01bfa0cc6aa4a210eb1302ec43bc62ff330
Amin Ibrahem <aibrahem1@sheffield.ac.uk>
2025-10-06 15:45:41 +0200

    stats theory 1

--- Script/pstats.py
+++ Script/pstats.py
@@ -3,6 +3,5 @@ print('Moad is playing PS5')
 
 from pydataset import data
 
-data().to_csv('Data/datasets_list.csv')
+data().to_csv('Data/datasets_list1.csv')
 data().to_excel('Data/datasets_list.xlsx')
-
